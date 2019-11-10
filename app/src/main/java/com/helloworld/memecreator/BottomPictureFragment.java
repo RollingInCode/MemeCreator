7d1e710262f4e4a4809164b109a6130ac35afbb4
Frank <franklyngonzalez94@gmail.com>
2019-11-10 01:37:02 -0500

    Initial commit

---
+++ app/src/main/java/com/helloworld/memecreator/BottomPictureFragment.java
@@ -0,0 +1,34 @@
+package com.helloworld.memecreator;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class BottomPictureFragment extends Fragment {
+
+    private static TextView topMemeText;
+    private static TextView bottomMemeText;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottom_picture_fragment, container, false);
+        topMemeText = (TextView) view.findViewById(R.id.topMemeText);
+        bottomMemeText = (TextView) view.findViewById(R.id.bottomMemeText);
+        return view;
+    }
+
+    public void setMemeText(String top, String bottom) {
+        topMemeText.setText(top);
+        bottomMemeText.setText(bottom);
+    }
+}
+
+
